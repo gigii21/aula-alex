b9afb70c0c48aac00ba501fe98c1417f4966eba7
gigii21 <128159936+gigii21@users.noreply.github.com>
2024-02-22 09:50:29 -0300

    Add files via upload

---
+++ typenumber.js
@@ -0,0 +1,10 @@
+// tipos numeros
+
+const meuNumero = 3;
+
+const number1 = 1;
+const number2 = 2;
+
+const operacao = number1 * number2;
+
+console.log(operacao);
---
+++ typestring.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senha";
+const stingDeNumeros = "34567"
+
+const citacao = 'Meu nome é';
+const seuNome = "Leonardo";
+
+
+// contatenação (+)
+
+console.log(citacao + seuNome)
+
+
+//template string OU template literal
